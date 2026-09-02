28ac74ba9a7dd7d94bbab769c0ed01a37e800aa3
Alv0804 <muhammadalvinapriliandi@gmail.com>
2026-09-02 16:52:23 +0700

    Upload Pertemuan 3

---
+++ Pertemuan 3/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtNilai = New TextBox()
+        btnInput = New Button()
+        pctImage = New PictureBox()
+        CType(pctImage, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtNilai
+        ' 
+        txtNilai.Location = New Point(304, 189)
+        txtNilai.Name = "txtNilai"
+        txtNilai.Size = New Size(150, 31)
+        txtNilai.TabIndex = 0
+        ' 
+        ' btnInput
+        ' 
+        btnInput.Location = New Point(326, 240)
+        btnInput.Name = "btnInput"
+        btnInput.Size = New Size(112, 32)
+        btnInput.TabIndex = 1
+        btnInput.Text = "Input"
+        btnInput.UseVisualStyleBackColor = True
+        ' 
+        ' pctImage
+        ' 
+        pctImage.Location = New Point(326, 66)
+        pctImage.Name = "pctImage"
+        pctImage.Size = New Size(100, 100)
+        pctImage.SizeMode = PictureBoxSizeMode.Zoom
+        pctImage.TabIndex = 2
+        pctImage.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(pctImage)
+        Controls.Add(btnInput)
+        Controls.Add(txtNilai)
+        Name = "Form1"
+        Text = "Form1"
+        CType(pctImage, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtNilai As TextBox
+    Friend WithEvents btnInput As Button
+    Friend WithEvents pctImage As PictureBox
+
+End Class
---
+++ Pertemuan 3/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        Dim nilaiUjian As Integer
+
+        If Not Integer.TryParse(txtNilai.Text, nilaiUjian) Then
+            MessageBox.Show("Masukkan nilai ujian yang valid.")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilaiUjian < 0 OrElse nilaiUjian > 100 Then
+            MessageBox.Show("Masukan nilai 0-100")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilaiUjian <= 50 Then
+            pctImage.Image = Image.FromFile("Assets\1.png")
+        ElseIf nilaiUjian <= 80 Then
+            pctImage.Image = Image.FromFile("Assets\2.png")
+        Else
+            pctImage.Image = Image.FromFile("Assets\3.png")
+        End If
+    End Sub
+
+    Private Sub txtNilai_TextChanged(sender As Object, e As EventArgs) Handles txtNilai.TextChanged
+
+    End Sub
+
+    Private Sub txtNilai_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilai.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub pctImage_Click(sender As Object, e As EventArgs) Handles pctImage.Click
+
+    End Sub
+End Class
